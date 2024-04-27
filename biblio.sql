c9241fca29a7abcb8a7d7b1aa69f4bbba2da752c
BertrandBENE <bertradmengouna@gmail.com>
2024-04-27 16:47:50 +0200

    git pull origin main

--- biblio.sql
+++ biblio.sql
@@ -1,28 +1,26 @@
 create database biblio;
 
-use bibliotheque;
+use biblio;
 
 create table etudiant(
-    codeEtudiant int (50) primary key auto_increment,
+    codeEtudiant int (50) primary key,
     nom varchar (60) not null,
     prenom varchar (50) default null,
     adresse varchar (50) not null,
     classe varchar (20) not null,
-    photoEtudiant blob not null
-);
+)engine = InnoDB default charset = utf8;
 
 create table livre(
-    codeLivre int (50) primary key auto_increment,
+    codeLivre int (50) primary key,
     titre varchar (60) not null,
     auteur varchar (60) not null,
-    dateEdition date default null,
-    photoLivre blob not null
-);
+    dateEdition date,
+)engine = InnoDB default charset = utf8;
 
 create table emprunter (
     codeEtudiant int (50),
     codeLivre int (50),
-    dateEmprunt TIMESTAMP,
+    dateEmprunt date not null,
     foreign key (codeEtudiant) references etudiant (codeEtudiant),
     foreign key (codeLivre) references livre (codeLivre)
 ) engine = InnoDB default charset = utf8;
